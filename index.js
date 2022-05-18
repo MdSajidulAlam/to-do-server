04963143f5b8e84f0be905fb9fd68f76e6048a47
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-19 03:25:10 +0600

    delete task implemented

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -31,6 +31,12 @@ async function run() {
             res.send(myTask)
         })
 
+        app.delete('/task/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await taskCollection.deleteOne(query)
+            res.send(result)
+        })
 
     }
     finally {
